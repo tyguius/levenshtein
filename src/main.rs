8d8a2dcc76b61d9cab698f21ef8e243320e482ff
tyguius <ta.tgyns13@gmail.com>
2023-01-24 07:50:57 +0100

    morgens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-    println!("Hello, world!");la
+    //println!("Hello, world!");
+    const word1: &str = "Wort1";
+    const word2: &str = "Wort2";
+    println!("{:?}", word1.chars());
+    println!("{:?}", word2.chars());
+    const len_word1: usize = word1.len();
+    const len_word2: usize = word2.len();
+    println!("{}", len_word1);
+    println!("{}", len_word2);
+    let mut table = [[0u8;len_word1 + 1]; len_word2 + 1];
+    //table[0][1] = 42;
+    println!("{:#?}", &table);
+
 
 }
